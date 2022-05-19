2f9ba8f7a28684eeb1e7c8ad7c8aaeced5bd1560
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-05-19 23:04:11 +0200

    esercizio completo(diciamo)

--- js/app.js
+++ js/app.js
@@ -17,3 +17,58 @@ minorenne
 maggiorenne
 over 65
 In base alla voce scelta dall’utente, fare i dovuti calcoli*/
+
+const inputName = document.querySelector("[name='userName']");
+const inputKm = document.querySelector("[name='km']");
+const inputEta = document.querySelector("[name='eta']");
+// let biglietto = document.querySelector(".biglietto-finale")
+const generateButton = document.getElementById("generateButton");
+const deleteButton = document.getElementById("deleteButton");
+
+deleteButton.addEventListener("click", function () {
+    window.location.reload();
+});
+
+generateButton.addEventListener("click", function () {
+
+    let userName = document.getElementById("userName");
+    // @ts-ignore
+    userName.innerHTML = inputName.value;
+    
+    const prezzoAlKm = 0.21;
+    // @ts-ignore
+    let quantitaKm = inputKm.value;
+    // @ts-ignore
+    let eta = inputEta.value;
+    let prezzoBiglietto = parseInt(quantitaKm) * prezzoAlKm;
+    let prezzoFinale;
+
+    if (parseInt(eta) < 18) {
+    (prezzoFinale = prezzoBiglietto - (prezzoBiglietto * 20 / 100));
+}   else if (parseInt(eta) > 65) {
+    (prezzoFinale = prezzoBiglietto - (prezzoBiglietto * 40 / 100));
+}   else {
+    (prezzoFinale = prezzoBiglietto);
+};
+    let finalPrice = document.getElementById("finalPrice");
+    finalPrice.innerHTML = `${prezzoFinale.toFixed(2)}€`;
+
+    let offertType = document.getElementById("offertType")
+    if (parseInt(eta) < 18) {
+        offertType.innerHTML = "Biglietto ridotto minorenni";
+    }   else if (parseInt(eta) > 65) {
+        offertType.innerHTML = "Biglietto ridotto over-65";
+    }   else {
+        offertType.innerHTML = "Biglietto standard";
+    };
+
+    let wagon = document.getElementById("wagon");
+    // @ts-ignore
+    wagon.innerHTML = Math.floor(Math.random() *5) +1;
+
+    let cpCode = document.getElementById("cpCode");
+    // @ts-ignore
+    cpCode.innerHTML = Math.floor(Math.random() *9999) +1;
+})
+
+
